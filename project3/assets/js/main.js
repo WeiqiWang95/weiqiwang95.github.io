72127444b3e065d496f1af7337214b9a0b0202d3
WeiqiWang95 <wwan205@student.monash.edu>
2017-05-19 16:07:25 +1000

    5/19

---
+++ project3/assets/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+
+
+  $('.cloud').mouseenter(function() {
+     $('.footnote').addClass('showme');
+   });
+
+   $('.cloud').mouseleave(function() {
+     $('.footnote').removeClass('showme');
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
