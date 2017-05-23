a72897a21d63fe8a78f74ce307a56cf74e7b1e95
WeiqiWang95 <wwan205@student.monash.edu>
2017-05-23 12:50:13 +1000

    5/23

--- project3/assets/js/main.js
+++ project3/assets/js/main.js
@@ -10,6 +10,18 @@ $(document).ready(function() {
      $('.footnote').removeClass('showme');
    });
 
+// toggleClass adds the color green on the first click, and then removes it.
+   $('.image1').click(function(){
+  $('.paragraph1').toggleClass('green');
+
+  $('.image2').click(function(){
+ $('.paragraph2').addClass('blue');
+});
+
+$('.image3').click(function(){
+ $('.paragraph3').addClass('red');
+});
+
 
 
 
